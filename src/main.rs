9479c6fd76c197727d94b6831b90710983cff680
userdrift <robertwindowshost@gmail.com>
2024-08-30 18:07:23 +0300

    remove api key

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,39 @@
 use reqwest;
 use serde_json::Value;
 use std::fs;
+use std::io::{self, Write};
 use std::path::Path;
+
+const SECRET_KEY: &str = "SECRET_KEY";
+
+fn fetch_() -> Result<String, io::Error> {
+    if Path::new(SECRET_KEY).exists() {
+        fs::read_to_string(SECRET_KEY)
+    } else {
+        print!("API_KEY: ");
+        io::stdout().flush()?;
+        let mut secret_key = String::new();
+        io::stdin().read_line(&mut secret_key)?;
+        let secret_key = secret_key.trim().to_string();
+        fs::write(SECRET_KEY, &secret_key)?;
+        Ok(secret_key)
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let jśon = "request.json";
-    if !Path::new(jśon).exists() {
+    let json_file = "request.json";
+    if !Path::new(json_file).exists() {
+        println!("request.json file not found.");
         return Ok(());
     }
 
-    let content = fs::read_to_string(jśon)?;
+    let secret_key = fetch_()?;
+
+    let content = fs::read_to_string(json_file)?;
     let value: Value = serde_json::from_str(&content)?;
 
     let client = reqwest::Client::new();
-    let secret_key = "AIzaSyCh_C0LtMD_fOG-T_7qtjm2HhfFqCCGv2U";
     let url = format!(
         "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={}",
         secret_key
@@ -31,10 +50,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     if let Some(candidates) = response["candidates"].as_array() {
         if let Some(first_candidate) = candidates.first() {
             if let Some(content) = first_candidate["content"]["parts"][0]["text"].as_str() {
-                if let Some(fortune_start) = content.find("\"value\":") {
-                    let fortune_content = &content[fortune_start + "\"value\":".len()..];
-                    if let Some(fortune_end) = fortune_content.find('}') {
-                        let fortune = fortune_content[..fortune_end].trim();
+                if let Some(fortune_s) = content.find("\"value\":") {
+                    let fortune_content = &content[fortune_s + "\"value\":".len()..];
+                    if let Some(fortune_e) = fortune_content.find('}') {
+                        let fortune = fortune_content[..fortune_e].trim();
                         println!("{}", fortune.trim_matches('"'));
                     }
                 }
